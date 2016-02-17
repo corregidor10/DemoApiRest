19dec8617e67ae80e2c18a6defabeba16309a6d2
Daniel Corregidor Coronado <Daniel.Corregidor@tajamar365.com>
2016-02-17 12:05:31 +0100

    con odata

--- DemoApiRest/App_Start/WebApiConfig.cs
+++ DemoApiRest/App_Start/WebApiConfig.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.OData.Builder;
+using System.Web.Http.OData.Extensions;
 using DemoApiRest.Extensiones;
+using DemoApiRest.Models;
 
 namespace DemoApiRest
 {
@@ -10,11 +13,18 @@ namespace DemoApiRest
     {
         public static void Register(HttpConfiguration config)
         {
-            //TODO 002. Registramos el Manejador de Mensajes que acabamos de crear.
+            //TODO MESSAGEHANDLERS 003. Registramos el Manejador de Mensajes que acabamos de crear.
             // Web API configuration and services
             config.MessageHandlers.Add(new LogHandler());
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+          // TODO ODATA 003. COPIAMOS LAS 4 lineas de Abajo en el Web Api Config
+
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Usuario>("Usuarios");
+            builder.EntitySet<Mensaje>("Mensaje");
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
--- DemoApiRest/Extensiones/LogHandler.cs
+++ DemoApiRest/Extensiones/LogHandler.cs
@@ -8,8 +8,8 @@ namespace DemoApiRest.Extensiones
 {
     public class LogHandler:DelegatingHandler
     {
-         //TODO 001. Creamos un metodo para capturar las peticiones que llegan a la API.
-         
+        //TODO MESSAGEHANDLERS 001. Creamos un metodo para capturar las peticiones que llegan a la API.
+
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken tokencancelacion)
         {
             using (var d=File.AppendText(@"c:\log\logapi.txt"))
@@ -17,8 +17,10 @@ namespace DemoApiRest.Extensiones
                 d.WriteLine("{0:d} Request-> {1}", DateTime.Now, request);
             }
 
-            //todo 002 en el return le ponemos en el base para que llame al padre.
+            //TODO MESSAGEHANDLERS 002 en el return le ponemos en el base para que llame al padre.
             return base.SendAsync(request, tokencancelacion);
         }
     }
 }
+
+//TODO ODATA 001. IMmplementamos el Modelo de Datos
