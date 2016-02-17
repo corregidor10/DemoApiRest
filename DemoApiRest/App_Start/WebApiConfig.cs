5d2916e70e924ff7ac66d35f9d384c9014700dbf
Daniel Corregidor Coronado <Daniel.Corregidor@tajamar365.com>
2016-02-17 10:37:16 +0100

    001

---
+++ DemoApiRest/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using DemoApiRest.Extensiones;
+
+namespace DemoApiRest
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //TODO 002. Registramos el Manejador de Mensajes que acabamos de crear.
+            // Web API configuration and services
+            config.MessageHandlers.Add(new LogHandler());
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ DemoApiRest/Extensiones/LogHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoApiRest.Extensiones
+{
+    public class LogHandler:DelegatingHandler
+    {
+         //TODO 001. Creamos un metodo para capturar las peticiones que llegan a la API.
+         
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken tokencancelacion)
+        {
+            using (var d=File.AppendText(@"c:\log\logapi.txt"))
+            {
+                d.WriteLine("{0:d} Request-> {1}", DateTime.Now, request);
+            }
+
+            //todo 002 en el return le ponemos en el base para que llame al padre.
+            return base.SendAsync(request, tokencancelacion);
+        }
+    }
+}
